c184bf0bad52faafa6c9f090eaa92f8182eab342
Markus Warg <mw@it-sls.de>
2010-08-12 09:49:18 +0200

    source code taken from cacert-20100728.tar.bz2

---
+++ www/policy/RootDistributionLicense.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN"
+	"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+ <meta http-equiv="CONTENT-TYPE" content="text/html; charset=utf-8" />
+ <title> CAcert - Root Distribution License - DRAFT </title>
+<style type="text/css">  <!-- only for WIP -->
+<!--
+.change {
+        color : blue;
+        font-weight: bold;
+}
+.comment {
+        color : steelblue;
+}
+-->
+</style>
+
+</head>
+<body>
+
+<div class="comment">
+<table width="100%">
+
+<tr>
+<td>
+Name: RDL <a style="color: steelblue" href="https://svn.cacert.org/CAcert/Policies/ControlledDocumentList.html">COD14</a><br />
+Status: DRAFT <a style="color: steelblue" href="https://wiki.cacert.org/PolicyDecisions#p20100710">p20100710</a> <br />
+Editor: Mark Lipscombe<br />
+</td>
+<td align="right">
+  <a href="//www.cacert.org/policy/PolicyOnPolicy.php"><img src="/images/cacert-draft.png" alt="RDL Status - DRAFT" height="31" width="88" style="border-style: none;" /></a>
+
+</td>
+</tr>
+</table>
+</div>
+
+<p><br /><br /></p>
+
+<table border="1"><tr><td>
+
+<h1> Root Distribution License </h1>
+
+<h2 id="s1"> 1. Terms </h2>
+
+<p>
+"CAcert Inc" means CAcert Incorporated, a non-profit association incorporated in New South Wales, Australia.<br />
+"CAcert Community Agreement" means the agreement entered into by each person wishing to RELY. <br />
+"Member" means a natural or legal person who has agreed to the CAcert Community Agreement.<br />
+"Certificate" means any certificate or like device to which CAcert Inc's digital signature has been affixed.<br />
+"CAcert Root Certificates" means any certificate issued by CAcert Inc to itself for the purposes of signing further CAcert Roots or for signing certificates of Members.<br />
+"RELY" means the human act in taking on a risk or liability on the basis of the claim(s) bound within a certificate issued by CAcert.<br />
+"Embedded" means a certificate that is contained within a software application or hardware system, when and only when, that software application or system is distributed in binary form only.<br />
+</p>
+
+<h2 id="s2"> 2. Copyright </h2>
+
+<p>
+CAcert Root Certificates are Copyright CAcert Incorporated. All rights reserved.
+</p>
+
+<h2 id="s3"> 3. License </h2>
+
+<p>
+You may copy and distribute CAcert Root Certificates only in accordance with this license.
+</p>
+
+<p>
+CAcert Inc grants you a free, non-exclusive license to copy and distribute CAcert Root Certificates in any medium, with or without modification, provided that the following conditions are met:
+</p>
+
+<ul><li>
+    Redistributions of Embedded CAcert Root Certificates must take reasonable steps to inform the recipient of the disclaimer in section 4 or reproduce this license and copyright notice in full in the documentation provided with the distribution.
+  </li><li>
+    Redistributions in all other forms must reproduce this license and copyright notice in full.
+</li></ul>
+
+<h2 id="s4"> 4. Disclaimer </h2>
+
+<p>
+THE CACERT ROOT CERTIFICATES ARE PROVIDED "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED TO THE MAXIMUM EXTENT PERMITTED BY LAW. IN NO EVENT SHALL CACERT INC, ITS MEMBERS, AGENTS, SUBSIDIARIES OR RELATED PARTIES BE LIABLE TO THE LICENSEE OR ANY THIRD PARTY FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THESE CERTIFICATES, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. IN ANY EVENT, CACERT'S LIABILITY SHALL NOT EXCEED $1,000.00 AUSTRALIAN DOLLARS.
+</p>
+
+<p>
+THIS LICENSE SPECIFICALLY DOES NOT PERMIT YOU TO RELY UPON ANY CERTIFICATES ISSUED BY CACERT INC. IF YOU WISH TO RELY ON CERTIFICATES ISSUED BY CACERT INC, YOU MUST ENTER INTO A SEPARATE AGREEMENT WITH CACERT INC.
+</p>
+
+<h2 id="s5"> 5. Statutory Rights </h2>
+
+<p>
+Nothing in this license affects any statutory rights that cannot be waived or limited by contract. In the event that any provision of this license is held to be invalid or unenforceable, the remaining provisions of this license remain in full force and effect. 
+</p>
+
+</td></tr></table>
+
+<div class="comment">
+<h2> Alternatives </h2>
+
+<p>
+If you find the terms of the above
+Root Distribution License
+difficult or inadequate for your purposes, you may wish to:
+</p>
+
+<ul><li>
+    Enter into the CAcert Community Agreement by
+    <a href="https://www.cacert.org/index.php?id=1">
+    registering as a Member</a>.
+    This is free.
+  </li><li>
+    Delete CAcert Root Certificates from your software.
+    Your software documentation should give
+    directions and assistance for this.
+</li></ul>
+
+<p>
+These alternatives are outside the above
+Root Distribution License
+and do not incorporate.
+</p>
+</div>
+
+
+</body>
+</html>
