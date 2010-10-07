d407985f1916c3dbfa0182582908075d807ddefa
Markus Warg <markus@server12.mawa>
2010-10-07 09:50:50 +0200

    https://bugs.cacert.org/view.php?id=876 add descriptive text and link to RDL (cherry picked from commit 42cda067b4259989a3af6cb25bfac1dd5bc42e6a)

---
+++ www/policy/NRPDisclaimerAndLicence.php
@@ -0,0 +1,15 @@
+<?php
+loadem("index");
+$id = intval($id);
+//showheader(_("CAcert - Non-Related Persons - Disclaimer and Licence"));
+?>
+
+<table border="1" bgcolor="#EEEEEE"><tr><td>
+
+The document "Non Related Persons - Disclaimer And Licence" was replaced by the Root Distribution Licence, which can be found <a href="/policy/RootDistributionLicense.php">here</a>.
+
+</td>
+</tr>
+</table>
+</body>
+</html>
