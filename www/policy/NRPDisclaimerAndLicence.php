d385b7b2647355444a08b3b7f16924dc106f8a34
Michael Tänzer <neo@nhng.de>
2010-11-12 00:13:09 +0100

    Bug #876: NRP-DaL to be removed immediately
    - We don't need dynamic content here, so remove any PHP (as with the other
    policies)
    - Fix markup
    
    Signed-off-by: Michael Tänzer <neo@nhng.de>

--- www/policy/NRPDisclaimerAndLicence.php
+++ www/policy/NRPDisclaimerAndLicence.php
@@ -1,9 +1,8 @@
-<?php
-loadem("index");
-$id = intval($id);
-//showheader(_("CAcert - Non-Related Persons - Disclaimer and Licence"));
-?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 
+<html>
+<head><title>NRP-DAL was replaced by the Root Distribution License</title></head>
+<body>
 <table border="1" bgcolor="#EEEEEE"><tr><td>
 
 The document "Non Related Persons - Disclaimer And Licence" was replaced by the Root Distribution Licence, which can be found <a href="/policy/RootDistributionLicense.php">here</a>.
