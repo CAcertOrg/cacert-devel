65ecbdcf10c930f67cf42fde187ad73e14c94635
Bernhard Fröhlich <bernhard@cacert.org>
2011-07-13 00:11:30 +0200

    Added stype tags

--- pages/help/7.php
+++ pages/help/7.php
@@ -15,6 +15,7 @@
     along with this program; if not, write to the Free Software
     Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 */ ?>
+<p style="background-color: #FF8080; font-size: 150%">
 <?
 printf(_("This page has been moved to the %swiki%s. Please update your ".
 		"bookmarks and report any broken links."),
