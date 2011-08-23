80159772cbee942d5cdef18f992be3fcb4d092c6
Bernhard Fröhlich <bernhard@cacert.org>
2011-08-24 00:03:03 +0200

    Modifications by Uli, see https://bugs.cacert.org/view.php?id=957#c2309

--- pages/account/24.php
+++ pages/account/24.php
@@ -15,6 +15,16 @@
     along with this program; if not, write to the Free Software
     Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 */ ?>
+<?
+	// Reset session variables regarding Org's, present empty form	
+	if (array_key_exists('O',$_SESSION['_config']))         $_SESSION['_config']['O'] = "";
+  if (array_key_exists('contact',$_SESSION['_config']))   $_SESSION['_config']['contact'] = "";	
+  if (array_key_exists('L',$_SESSION['_config']))         $_SESSION['_config']['L'] = "";
+  if (array_key_exists('ST',$_SESSION['_config']))        $_SESSION['_config']['ST'] = "";
+  if (array_key_exists('C',$_SESSION['_config']))         $_SESSION['_config']['C'] = "";
+  if (array_key_exists('comments',$_SESSION['_config']))  $_SESSION['_config']['comments'] = "";
+	
+?>
 <form method="post" action="account.php">
 <table align="center" valign="middle" border="0" cellspacing="0" cellpadding="0" class="wrapper">
   <tr>
@@ -22,27 +32,27 @@
   </tr>
   <tr>
     <td class="DataTD"><?=_("Organisation Name")?>:</td>
-    <td class="DataTD"><input type="text" name="O" value="<?=array_key_exists('O',$_SESSION['_config'])?$_SESSION['_config']['O']:""?>" maxlength="50" size="90"></td>
+    <td class="DataTD"><input type="text" name="O" value="" maxlength="50" size="90"></td>
   </tr>
   <tr>
     <td class="DataTD"><?=_("Contact Email")?>:</td>
-    <td class="DataTD"><input type="text" name="contact" value="<?=array_key_exists('contact',$_SESSION['_config'])?$_SESSION['_config']['contact']:""?>" size="90"></td>
+    <td class="DataTD"><input type="text" name="contact" value="" size="90"></td>
   </tr>
   <tr>
     <td class="DataTD"><?=_("Town/Suburb")?>:</td>
-    <td class="DataTD"><input type="text" name="L" value="<?=array_key_exists('L',$_SESSION['_config'])?$_SESSION['_config']['L']:""?>" size="90"></td>
+    <td class="DataTD"><input type="text" name="L" value="" size="90"></td>
   </tr>
   <tr>
     <td class="DataTD"><?=_("State/Province")?>:</td>
-    <td class="DataTD"><input type="text" name="ST" value="<?=array_key_exists('ST',$_SESSION['_config'])?$_SESSION['_config']['ST']:""?>" size="90"></td>
+    <td class="DataTD"><input type="text" name="ST" value="" size="90"></td>
   </tr>
   <tr>
     <td class="DataTD"><?=_("Country")?>:</td>
-    <td class="DataTD"><input type="text" name="C" value="<?=array_key_exists('C',$_SESSION['_config'])?sanitizeHTML($_SESSION['_config']['C']):""?>" size="5">(2 letter <a href="http://www.iso.org/iso/english_country_names_and_code_elements">ISO code</a>)</td>
+    <td class="DataTD"><input type="text" name="C" value="" size="5">(2 letter <a href="http://www.iso.org/iso/english_country_names_and_code_elements">ISO code</a>)</td>
   </tr>
   <tr>
     <td class="DataTD"><?=_("Comments")?>:</td>
-    <td class="DataTD"><textarea name="comments" cols="60" rows="10"><?=array_key_exists('comments',$_SESSION['_config'])?$_SESSION['_config']['comments']:""?></textarea></td>
+    <td class="DataTD"><textarea name="comments" cols="60" rows="10"></textarea></td>
   </tr>
   <tr>
     <td class="DataTD" colspan="2"><input type="submit" name="process" value="<?=_("Next")?>"></td>
