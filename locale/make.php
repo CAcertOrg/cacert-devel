c99e1025667d8fef72b6fdbe7d34769a279a8942
Michael Tänzer <neo@nhng.de>
2011-09-28 00:00:25 +0200

    bug 985: add all possible locales and use new directory layout
    Signed-off-by: Michael Tänzer <neo@nhng.de>

--- locale/make.php
+++ locale/make.php
@@ -2,11 +2,13 @@
 <?
 	$lang = array(  "bg" => "bg_BG", "da" => "da_DK", "de" => "de_DE", "es" => "es_ES",
 			"fr" => "fr_FR", "fi" => "fi_FI", "he" => "he_IL", "hr" => "hr_HR",
-			"hu" => "hu_HU", "it" => "it_IT", "ja" => "ja_JP", "nl" => "nl_NL",
-			"pt" => "pt_PT", "ro" => "ro_RO", "ru" => "ru_RU", "fa" => "fa_IR",
-			"sv" => "sv_SE", "tr" => "tr_TR", "zh" => "zh_CN", "ar" => "ar_SY",
+			"hu" => "hu_HU", "id" => "id_ID", "it" => "it_IT", "ja" => "ja_JP",
+			"nl" => "nl_NL", "lv" => "lv_LV", "pt" => "pt_PT", "pt_BR" => "pt_BR",
+			"ro" => "ro_RO", "ru" => "ru_RU", "fa" => "fa_IR", "sv" => "sv_SE",
+			"tr" => "tr_TR", "zh_CN" => "zh_CN", "zh_TW" => "zh_TW", "ar" => "ar_SY",
 			"el" => "el_GR", "tl" => "tl_PH", "pl" => "pl_PL", "cs" => "cs_CZ",
-			"ka" => "ka_GE", "is" => "is_IS", "ko" => "ko_KR", "nb" => "nb_NO");
+			"ka" => "ka_GE", "is" => "is_IS", "ko" => "ko_KR", "nb" => "nb_NO",
+			"sl" => "sl_SI", "th" => "th_TH", "uk" => "uk_UA");
 
 	if($argc > 1)
 	{
@@ -14,21 +16,21 @@
 		{
 			$val = $lang[$key];
 			if($val != "")
-			{		
-				$do = `wget -O $key.po "http://translingo.cacert.org/export2.php?pid=1&editlanguage=$val" 2>&1`;
-echo $do;
+			{
 				echo `mkdir -p $key/LC_MESSAGES/`;
-				$do = `msgfmt -o $key/LC_MESSAGES/messages.mo $key.po 2>&1`;
+				$do = `wget -O $key/messages.po "http://translingo.cacert.org/export2.php?pid=1&editlanguage=$val" 2>&1`;
+echo $do;
+				$do = `msgfmt -o $key/LC_MESSAGES/messages.mo $key/messages.po 2>&1`;
 echo $do;
 			}
 		}
 	} else {
 		foreach($lang as $key => $val)
 		{
-		$do = `wget -O $key.po "http://translingo.cacert.org/export2.php?pid=1&editlanguage=$val" 2>&1`;
-echo $do;
 		echo `mkdir -p $key/LC_MESSAGES/`;
-		$do = `msgfmt -o $key/LC_MESSAGES/messages.mo $key.po 2>&1`;
+		$do = `wget -O $key/messages.po "http://translingo.cacert.org/export2.php?pid=1&editlanguage=$val" 2>&1`;
+echo $do;
+		$do = `msgfmt -o $key/LC_MESSAGES/messages.mo $key/messages.po 2>&1`;
 echo $do;
 		}
 	}
