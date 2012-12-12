e61716693f1f07487921958fe90056c57aa23f0e
INOPIAE <inopiae@cacert.org>
2012-12-12 01:19:16 +0100

    bug 1112: fixed typos

--- pages/wot/4.php
+++ pages/wot/4.php
@@ -38,41 +38,44 @@ Until a subsidiary policy under AP is written, it is against AP rules.<br>
 
 <? sprintf(_("If you are interested in the TTP programme, read the pages %s for the basic way how the TTP programme works for you, and %s whether the TTP programme affects the country where you are located."),"<a href='http://wiki.cacert.org/TTP/TTPuser'>http://wiki.cacert.org/TTP/TTPuser</a>","<a href='http://wiki.cacert.org/TTP/TTPAL'>http://wiki.cacert.org/TTP/TTPAL</a>")."\n\n" ?>
 
-                              
+<?
 // test for points <100
 if ($_SESSION['profile']['points']<100){
-  // test for TTP assurances
-  if (get_number_of_ttpassurances($userid)<2){
-    <p><?=_("If you want to ask for TTP assurances fill out the missing data and send the request to support@cacert.org to start the process. CAcert will inform you then about the next steps.)?></p>
-    <form method="post" action="wot.php">
-    <table align="center" valign="middle" border="0" cellspacing="0" cellpadding="0" class="wrapper">
-      <tr>
-        <td class="DataTD"><?=_("Country you whant to visit the TTP")?></td>
-        <td class="DataTD"><input type="text" name="country" size="30"></td>
-        //shall the input be replaced by a select, where should the country information be maintained
-      </tr>
-      <tr>
-        <td class="DataTD"><?=_("I want to take part in the TTP Topup Programme")?></td>
-        <td class="DataTD"><input type="checkbox" name="ttptopup" value="1"></td>
-      </tr>
-      <tr>
-        <td colspan="2" >
-          <input type="hidden" name="oldid" value="4">
-          <input type="submit" name="ttp" value="<?=_("I need a TTP assurance")?>">
-        </td>
-      </tr>
-    </table>
-   </form>
-  }else{
+	// test for TTP assurances
+	if (get_number_of_ttpassurances($userid)<2){?>
+		<p><?=_("If you want to ask for TTP assurances fill out the missing data and send the request to support@cacert.org to start the process. CAcert will inform you then about the next steps.)?></p>
+		<form method="post" action="wot.php">
+		<table align="center" valign="middle" border="0" cellspacing="0" cellpadding="0" class="wrapper">
+			<tr>
+				<td class="DataTD"><?=_("Country you whant to visit the TTP")?></td>
+				<td class="DataTD"><select size="1" name="country">
+						<option>Australia</option>
+						<option>Puerto Rico</option>
+						<option>USA</option>
+					</select></td>
+			</tr>
+			<tr>
+				<td class="DataTD"><?=_("I want to take part in the TTP Topup Programme")?></td>
+				<td class="DataTD"><input type="checkbox" name="ttptopup" value="1"></td>
+			</tr>
+			<tr>
+				<td colspan="2" >
+					<input type="hidden" name="oldid" value="4">
+					<input type="submit" name="ttp" value="<?=_("I need a TTP assurance")?>">
+				</td>
+			</tr>
+		</table>
+	</form>
+<?}else{?>
   /* As soon as the TPP TOPUP Programme is established this routine should be used
-    <p><?=_("As you got already 2 TTP assurances you only can take part in the TTP TOPUP programme.\n\n If you want to ask for the TTP TOPUP programme use the submit button to send the request to support@cacert.org to start the process. CAcert will inform you then about the next steps.")?></p>
-    <form method="post" action="wot.php">
-          <input type="hidden" name="oldid" value="<?=$id?>">
-          <input type="submit" name="ttptopup" value="<?=_("I need a TTP TOPUP")?>">
-   </form>
-  */
-    <p><?=_("We are working to develop the TTP TOPUP process to be able to fill the gap of the missing 30 assurance points to 100 assurance points. In the meanwhile you have to close this gap with face to face assurances with CAcert Assurers. Think not only travelling to populated countries, but as well to assurers visiting your country or area.")?></p>  
-  }
+	<p><?=_("As you got already 2 TTP assurances you only can take part in the TTP TOPUP programme.\n\n If you want to ask for the TTP TOPUP programme use the submit button to send the request to support@cacert.org to start the process. CAcert will inform you then about the next steps.")?></p>
+		<form method="post" action="wot.php">
+			<input type="hidden" name="oldid" value="<?=$id?>">
+			<input type="submit" name="ttptopup" value="<?=_("I need a TTP TOPUP")?>">
+	 </form>
+*/
+	<p><?=_("We are working to develop the TTP TOPUP process to be able to fill the gap of the missing 30 assurance points to 100 assurance points. In the meanwhile you have to close this gap with face to face assurances with CAcert Assurers. Think not only travelling to populated countries, but as well to assurers visiting your country or area.")?></p>  
+<?}?>
 
 }
 else{
