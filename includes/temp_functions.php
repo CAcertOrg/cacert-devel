1b792a921e43c1c3dc61da854d965a0bdd581e58
Benny Baumann <BenBE@geshi.org>
2013-01-07 17:45:13 +0100

    bug 893: Fix whitespace

--- includes/temp_functions.php
+++ includes/temp_functions.php
@@ -47,13 +47,13 @@ function account_domain_delete($domainid){
 		"update `domains`
 		set `deleted`=NOW()
 		where `id` = '$domainid'");
-
 }
 
 function account_delete($id, $arbno, $adminid){
 //deletes an account following the deleted account routnie V3 
 // called from www/account.php if($oldid == 50 && $process != "")
 //change password
+
 	$pool = 'abcdefghijklmnopqrstuvwxyz';
 	$pool .= '0123456789!()§';
 	$pool .= 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
@@ -64,16 +64,16 @@ function account_delete($id, $arbno, $adminid){
 		$password .= substr($pool,(rand()%(strlen ($pool))), 1);
 	}
 	mysql_query("update `users` set `password`=sha1('".$password."') where `id`='".$id."'");
-	
+
 //create new mail for arbitration number
 	$query = "insert into `email` set `email`='".$arbno."@cacert.org',`memid`='".$id."',`created`=NOW(),`modified`=NOW(), `attempts`=-1";
 	mysql_query($query);
 	$emailid = mysql_insert_id();
-	
+
 //set new mail as default
 	$query = "update `users` set `email`='".$arbno."@cacert.org' where `id`='".$id."'";
 	mysql_query($query);
-	
+
 //delete all other email address
 	$query = "select * from `email` where `memid`='".$id."' and `memid`='".$emailid."'" ;
 	$res=mysql_query($query);
@@ -87,26 +87,27 @@ function account_delete($id, $arbno, $adminid){
 	while($row = mysql_fetch_assoc($res)){}
 		account_email_delete($row['id']);
 	}
+
 //clear alert settings
 	mysql_query("update `alerts` set `general`='1' where `memid`='$id'");
 	mysql_query("update `alerts` set `country`='1' where `memid`='$id'");
 	mysql_query("update `alerts` set `regional`='1' where `memid`='$id'");
 	mysql_query("update `alerts` set `radius`='1' where `memid`='$id'");
-	
+
 //set default location
 	$query = "update `users` set `locid`='2256755', `regid`='243', `ccid`='12' where `id`='".$id."'";
 	mysql_query($query);
-	
+
 //clear listings
 	$query = "update `users` set `listme`=' ',`contactinfo`=' ' where `id`='".$id."'";
 	mysql_query($query);
-	
+
 //set lanuage to default
 	//set default language
 	mysql_query("update `users` set `language`='en_AU' where `id`='".$id."'");
 	//delete secondary langugaes
 	mysql_query("delete from `addlang` where `userid`='".$id."'");
-	
+
 //change secret questions 
 	for($i=1;$i<=5;$i++){}
 		$q="";
@@ -135,7 +136,7 @@ function account_delete($id, $arbno, $adminid){
 		`dob`='1900-01-01'
 		where `id`='".$id."'";
 	mysql_query($query);
- 	
+
 //clear all admin flags
 	mysql_query("update `users` set `assurer`='0' where `id`='$id'");
 	mysql_query("update `users` set `assurer_blocked`='0' where `id`='$id'");
@@ -146,8 +147,9 @@ function account_delete($id, $arbno, $adminid){
 	mysql_query("update `users` set `admin`='0' where `id`='$id'");
 	mysql_query("update `users` set `adadmin`='0' where `id`='$id'");
 	mysql_query("update `users` set `tverify`='0' where `id`='$id'");
-	
+
 //block account
 	mysql_query("update `users` set `locked`='1' where `id`='$id'");
 }
+
 ?>
