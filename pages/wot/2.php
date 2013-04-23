0649ccb54f3be7ecf7dfdb410639533f5e3f49cb
INOPIAE <inopiae@cacert.org>
2013-04-23 21:46:41 +0200

    bug 740: changed wording

--- pages/wot/2.php
+++ pages/wot/2.php
@@ -19,13 +19,15 @@
 <h3><?=_('To become an Assurer')?></h3>
 
 <p><?=_('To become a CAcert Assurer you need to fulfill the following requirements:')?></p>
-<p><?=_('1. you have to reach 100 Assurance points')?><br>
-<?=_('2. you have to pass the CATS tests')?></p>
+<ol>
+	<li><?=_('1. you have to reach 100 Assurance points')?></li>
+	<li><?=_('2. you have to pass the CATS test')?></li>
+</ol>
 
-<p><?=_('To reach 100 Assurance Points you have to meet with assurers who assures you under the CAcert Assurance Programme according to Assurance Policy. During the face to face meeting you need to show at least one governmental issued photo ID.')?></p>
+<p><?=_('To reach 100 Assurance Points you have to meet with assurers who assure you under the CAcert Assurance Programme according to Assurance Policy. During the face to face meeting you need to show at least one governmental issued photo ID.')?></p>
 
 <p><?=sprintf(_('The 2nd requirement in becoming assurer is to pass the CAcert Assurer Challenge, which can be started at %s. For more information explore the wiki regarding the %s.'),'<a href="https://cats.cacert.org">https://cats.cacert.org</a>','<a href="//wiki.cacert.org/AssurerChallenge">Assurer Challenge</a>')?><p>
 
-<p><?=sprintf(_('In case you cannot meet an Assurer (eg there are not many assurer in your area) you can becoming assured under the alternate %sTTP-assisted-assurance programme%s. Read the pages %s for the basic way how the TTP-assisted-assurance programme works for you and %s whether the TTP programme affects the country where you are located.'),'<a href="/wot.php?id=4">', '</a>','<a href="//wiki.cacert.org/TTP/TTPuser">TTPuser</a>','<a href="//wiki.cacert.org/TTP/TTPAL">TTPAL</a>')?> </p>
+<p><?=sprintf(_('In case you cannot meet an Assurer (eg there are not many assurer in your area) you can be assured under the alternate %sTTP-assisted-assurance programme%s. Read the pages %s for the basic way how the TTP-assisted-assurance programme works for you and %s whether the TTP programme affects the country where you are located.'),'<a href="/wot.php?id=4">', '</a>','<a href="//wiki.cacert.org/TTP/TTPuser">TTPuser</a>','<a href="//wiki.cacert.org/TTP/TTPAL">TTPAL</a>')?> </p>
 
 
