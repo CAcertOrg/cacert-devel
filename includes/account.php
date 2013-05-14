45ee203868aa194b242293102d96d5ef71f4a37c
INOPIAE <inopiae@cacert.org>
2013-05-14 21:50:28 +0200

    bug 782: removed echos and fixed variable name

--- includes/account.php
+++ includes/account.php
@@ -1136,7 +1136,6 @@
 	if($oldid == 5 && array_key_exists('change',$_REQUEST) && $_REQUEST['change'] != "")
 	{
 		showheader(_("My CAcert.org Account!"));
-echo var_dump($_REQUEST).'<br>';
 		foreach($_REQUEST as $id => $val)
 		{
 			if(substr($id,0,5)=="cert_")
@@ -1148,8 +1147,7 @@ echo var_dump($_REQUEST).'<br>';
 			if(substr($id,0,14)=="check_comment_")
 			{
 				$cid = intval(substr($id,14));
-echo 'cid:'.$cid.'<br>';
-				if (!empty($_REQUEST['check_comment_'.$id])) {
+				if(!empty($_REQUEST['check_comment_'.$cid])) {
 					$comment=trim(mysql_real_escape_string(stripslashes($_REQUEST['comment_'.$cid])));
 					mysql_query("update `emailcerts` set `description`='$comment' where `id`='$cid' and `memid`='".$_SESSION['profile']['id']."'");
 				}
