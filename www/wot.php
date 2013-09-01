a4ad0eda59ead11d708b96eb67dc657a4429ba8c
Bernhard Fröhlich <bernhard@cacert.org>
2013-09-01 17:29:41 +0200

    Replaced "Trusted 3rd Parties" by "Trusted Third Parties", "Trusted 3rd Parties" is not documented

--- www/wot.php
+++ www/wot.php
@@ -278,7 +278,7 @@ $iecho= "c";
 		}
 
 		//met assuree in person, not appliciable fot TTP / TTP Topup assurances
-		if((!array_key_exists('certify',$_POST) || $_POST['certify'] != 1 )  && $_REQUEST['method'] != "Trusted 3rd Parties")
+		if((!array_key_exists('certify',$_POST) || $_POST['certify'] != 1 )  && $_REQUEST['method'] != "Trusted Third Parties")
 		{
 			show_page("VerifyData","",_("You failed to check all boxes to validate your adherence to the rules and policies of CAcert"));
 			exit;
