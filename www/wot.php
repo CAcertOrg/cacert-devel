24603f28cde091813d8022daa9153eb4ab685ca4
Benny Baumann <BenBE@geshi.org>
2013-09-03 23:31:02 +0200

    bug 1137: Revert patch from Ted, because of inconsistencies. Also fix a typo in the comment above

--- www/wot.php
+++ www/wot.php
@@ -277,8 +277,8 @@ $iecho= "c";
 			exit;
 		}
 
-		//met assuree in person, not appliciable fot TTP / TTP Topup assurances
-		if((!array_key_exists('certify',$_POST) || $_POST['certify'] != 1 )  && $_REQUEST['method'] != "Trusted Third Parties")
+		//met assuree in person, not appliciable for TTP / TTP Topup assurances
+		if((!array_key_exists('certify',$_POST) || $_POST['certify'] != 1 )  && $_REQUEST['method'] != "Trusted 3rd Parties")
 		{
 			show_page("VerifyData","",_("You failed to check all boxes to validate your adherence to the rules and policies of CAcert"));
 			exit;
