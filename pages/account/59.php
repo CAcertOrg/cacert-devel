84814bff4a82dcdacd181396529b9c8943c73318
mam <m.maengel@project-biz.de>
2014-02-21 07:57:14 +0100

    bug 1138: added footer output if ticket no missing for pages/account/44.php and pages/account/59.php

--- pages/account/59.php
+++ pages/account/59.php
@@ -47,6 +47,7 @@ if(intval($_REQUEST['oldid'])==43){
 $ticketno = ""; if(array_key_exists('ticketno', $_SESSION)) $ticketno = $_SESSION['ticketno'];
 if (!valid_ticket_number($ticketno) && $support == 1) {
     echo printf(_("I'm sorry, you did not enter a ticket number!%sSupport is not allowed to view the account history without a ticket number.%s"), '<br/>', '<br/><a href="account.php?id=43&amp;userid=' . intval($_REQUEST['userid']) .'">'. _('Back to previous page.').'</a>');
+    showfooter();
     exit;
 }
 ?>
