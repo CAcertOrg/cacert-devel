643222269fab0bd19f765ccbef4e199b047c549e
INOPIAE <inopiae@cacert.org>
2014-02-28 09:08:47 +0100

    bug 649: indented html part

--- pages/wot/16.php
+++ pages/wot/16.php
@@ -22,7 +22,7 @@ require_once("../includes/lib/l10n.php");
 if(array_key_exists('error',$_SESSION['_config']) && $_SESSION['_config']['error'] != "")
 {
     ?><font color="orange" size="+1">
-      <? echo _("ERROR").": ".$_SESSION['_config']['error'] ?>
+        <? echo _("ERROR").": ".$_SESSION['_config']['error'] ?>
     </font>
     <?unset($_SESSION['_config']['error']);
 }
@@ -31,44 +31,44 @@ if(array_key_exists('error',$_SESSION['_config']) && $_SESSION['_config']['error
     <? unset($_SESSION['_config']['noemailfound']); } ?>
 <form method="post" action="wot.php" name="form1">
 <table align="center" valign="middle" border="0" cellspacing="0" cellpadding="0" class="wrapper">
-  <tr>
-    <td colspan="2" class="title"><?=_('Check Assurer Status')?></td>
-  </tr>
-  <tr>
+    <tr>
+        <td colspan="2" class="title"><?=_('Check Assurer Status')?></td>
+    </tr>
+    <tr>
     <td class="DataTD"><?=_('Email to check')?>:</td>
 <? if(array_key_exists('remindersent',$_SESSION['_config']) && $_SESSION['_config']['remindersent'] == 1) { unset($_SESSION['_config']['remindersent']) ?>
-    <td class="DataTD"><input type="text" name="email" id="email" value=""></td>
-    <? } else { ?>
-    <td class="DataTD"><input type="text" name="email" id="email" value="<?=array_key_exists('email',$_POST)?sanitizeHTML($_POST['email']):""?>"></td>
-        <? } ?>
-  </tr>
-  <tr>
-    <td class="DataTD"><?=_('Reason why the assurer status is needed')?>:</td>
-    <td class="DataTD"><select name="reason"><option>--</option>
-            <option><?=_('Assurance')?></option>
-            <option><?=_('Event Preparation')?></option>
-            <option><?=_('Arbitration')?></option>
-            <option><?=_('CARS check')?></option>
-            <option><?=_('CATS certificate creation')?></option>
-            <option><?=_('Organisation Assurance')?></option>
-        </select></td>
-  </tr>
-  <tr>
-    <td class="DataTD" colspan="2"><?=_('If you request the assurer status the result is send to yourself as well as to the person of whom you request the status of.')?></td>
-  </tr>
-  <tr>
-    <td class="DataTD" colspan="2"><input type="submit" name="process" value="<?=_("Next")?>"></td>
-  </tr>
+        <td class="DataTD"><input type="text" name="email" id="email" value=""></td>
+        <? } else { ?>
+        <td class="DataTD"><input type="text" name="email" id="email" value="<?=array_key_exists('email',$_POST)?sanitizeHTML($_POST['email']):""?>"></td>
+            <? } ?>
+    </tr>
+    <tr>
+        <td class="DataTD"><?=_('Reason why the assurer status is needed')?>:</td>
+        <td class="DataTD"><select name="reason"><option>--</option>
+                <option><?=_('Assurance')?></option>
+                <option><?=_('Event Preparation')?></option>
+                <option><?=_('Arbitration')?></option>
+                <option><?=_('CARS check')?></option>
+                <option><?=_('CATS certificate creation')?></option>
+                <option><?=_('Organisation Assurance')?></option>
+            </select></td>
+    </tr>
+    <tr>
+        <td class="DataTD" colspan="2"><?=_('If you request the assurer status the result is send to yourself as well as to the person of whom you request the status of.')?></td>
+    </tr>
+    <tr>
+        <td class="DataTD" colspan="2"><input type="submit" name="process" value="<?=_("Next")?>"></td>
+    </tr>
 </table>
 <input type="hidden" name="oldid" value="<?=$id?>">
 </form>
 <SCRIPT LANGUAGE="JavaScript">
 //<![CDATA[
-	function my_init()
-	{
-		document.getElementById("email").focus();
-	}
+    function my_init()
+    {
+        document.getElementById("email").focus();
+    }
 
-	window.onload = my_init();
+    window.onload = my_init();
 //]]>
 </script>
