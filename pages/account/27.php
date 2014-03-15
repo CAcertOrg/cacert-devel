1fe48d41048314e6f858a682018a026294d76431
INOPIAE <inopiae@cacert.org>
2014-03-15 21:15:44 +0100

    bug 657: added comments to forms

--- pages/account/27.php
+++ pages/account/27.php
@@ -21,23 +21,27 @@
 <form method="post" action="account.php">
 <table align="center" valign="middle" border="0" cellspacing="0" cellpadding="0" class="wrapper">
   <tr>
-    <td colspan="2" class="title"><?=_("Edit Organisation")?></td>
+    <td colspan="3" class="title"><?=_("Edit Organisation")?></td>
   </tr>
   <tr>
     <td class="DataTD"><?=_("Organisation Name")?>:</td>
     <td class="DataTD"><input type="text" name="O" value="<?=$row['O']?>" size="90"></td>
+    <td class="DataTD"><?=_("max 64 characters")?></td>
   </tr>
   <tr>
     <td class="DataTD"><?=_("Contact Email")?>:</td>
     <td class="DataTD"><input type="text" name="contact" value="<?=($row['contact'])?>" size="90"></td>
+    <td class="DataTD"><?=_("max 255 characters")?></td>
   </tr>
   <tr>
     <td class="DataTD"><?=_("Town/Suburb")?>:</td>
     <td class="DataTD"><input type="text" name="L" value="<?=($row['L'])?>" size="90"></td>
+    <td class="DataTD"><?=_("max 128 characters")?></td>
   </tr>
   <tr>
     <td class="DataTD"><?=_("State/Province")?>:</td>
     <td class="DataTD"><input type="text" name="ST" value="<?=($row['ST'])?>" size="90"></td>
+    <td class="DataTD"><?=_("max 128 characters")?></td>
   </tr>
   <tr>
     <td class="DataTD"><?=_("Country")?>:</td>
@@ -46,13 +50,15 @@
             '<a href="http://www.iso.org/iso/home/standards/country_codes/iso-3166-1_decoding_table.htm">',
             '</a>')?>
     </td>
+    <td class="DataTD"><?=_("2 characters")?></td>
   </tr>
   <tr>
     <td class="DataTD"><?=_("Comments")?>:</td>
     <td class="DataTD"><textarea name="comments" cols=60 rows=10><?=($row['comments'])?></textarea></td>
+    <td class="DataTD">&nbsp</td>
   </tr>
   <tr>
-    <td class="DataTD" colspan="2"><input type="submit" name="process" value="<?=_("Update")?>"></td>
+    <td class="DataTD" colspan="3"><input type="submit" name="process" value="<?=_("Update")?>"></td>
   </tr>
 </table>
 <input type="hidden" name="oldid" value="<?=intval($id)?>">
