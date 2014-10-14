58d2ce2b4829bb3869f7fd9c361358596c2a709e
INOPIAE <inopiae@cacert.org>
2014-10-14 21:59:57 +0200

    bug-1192: changed text on index/52.php

--- pages/index/52.php
+++ pages/index/52.php
@@ -22,12 +22,12 @@
 	<h1><?=_('CAcert Community Agreement Acceptance')?></h1>
 	<p><?=sprintf(_('To get access to your account your agreement to the %s CAcert Community Agreement %s (CCA) is required.'),'<a href="/policy/CAcertCommunityAgreement.php">', '</a>')?></p>
 	<p><?=_('Every member, who has agreed to the CCA, should be able to rely on the fact that every other user of CAcert has also agreed to the CCA and that the same rules apply to everybody. Moreover it is a basic requirement for the audit to be able to tell who has accepted our rules.')?></p>
-	<p><?=_('Originally the acceptance was not recorded. Once you have accepted the CCA with the link below, your agreement is recorded in the database and you will not need to do this step again.')?></p>
-	<p><?=sprintf(_('If you do not wish to accept the CCA you should consider to ask for the closing of your account as you will not be able to access it any longer. In this case please send an email to support (%s).'),'<a href="mailto:support@cacert.org">support@cacert.org</a>')?></p>
+	<p><?=_('Originally the acceptance was not recorded. Up until now, we do not have your agreement on record. Once you have accepted the CCA (again) your agreement is recorded and you will not need to do this step again.')?></p>
+	<p><?=sprintf(_('If you do not wish to accept the CCA you should consider to ask for the closing of your account as you will not be able to access our system. In this case please send an email to support (%s).'),'<a href="mailto:support@cacert.org">support@cacert.org</a>')?></p>
+	<p><?=_('If you do not want to decide about the acceptance of the CCA now, you can come back at any time.')?></p>
 	<form method="post" action="index.php">
 		<input type="submit" name="agree" value="<?=_('I agree to the CCA')?>">
 		<input type="submit" name="disagree" value="<?=_('I do not want to accept the CCA')?>">
-
 		<input type="hidden" name="oldid" value="<?=$id?>">
 	</form>
 </div>
