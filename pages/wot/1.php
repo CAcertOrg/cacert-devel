64c93bfd528d95436b3081e76bf8e5174936c6aa
Felix Dörre <felix@dogcraft.de>
2015-04-07 22:47:19 +0200

    bug-1060: new points calculation for pages/wot/1.php

--- pages/wot/1.php
+++ pages/wot/1.php
@@ -92,7 +92,7 @@
 	$query = "select *, `users`.`id` as `id` from `users`,`notary` where `listme`='1' and
 			`ccid`='".$ccid."' and `regid`='".$regid."' and
 			`locid`='".$locid."' and `users`.`id`=`notary`.`to` and `notary`.`deleted`=0
-			group by `notary`.`to` HAVING SUM(`points`) >= 100 order by `points` desc";
+			group by `notary`.`to` HAVING SUM(`awarded`) >= 100 order by `points` desc";
 	$list = mysql_query($query);
 	if(mysql_num_rows($list) > 0)
 	{
