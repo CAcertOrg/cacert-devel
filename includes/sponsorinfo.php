1c1cf311520ea19bf711ad955eba59eceaf9683e
Karl-Heinz Gödderz (GuKKDevel) <Devel@GuKK-Online.de>
2018-11-06 11:41:57 +0100

    Bug-1423; delete a sponsor-link

--- includes/sponsorinfo.php
+++ includes/sponsorinfo.php
@@ -3,6 +3,5 @@
     <a href="http://www.bit.nl/" target="_blank"><img class="sponsorlogo" src="/images/bit.png" alt="[BIT logo]" border="0"></a>
     <a href="http://www.tunix.nl/" target="_blank"><img class="sponsorlogo" src="/images/tunix.png" alt="[TUNIX logo]" border="0"></a>
     <a href="http://www.nlnet.nl/" target="_blank"><img class="sponsorlogo" src="/images/nlnet.png" alt="[NLnet logo]" border="0"></a>
-    <a href="http://www.openarchitecturenetwork.org/" target="_blank"><img class="sponsorlogo" src="/images/oan.png" alt="[OAN logo]" border="0"></a>
   </div>
 
